0dee69213881bbd6c453459e5d3266fbb04a7ea9
KatoMono Enkeli <k4t0mono@gmail.com>
2018-04-25 18:08:38 -0300

    Lecture 009: Setting up Babel

--- section03/src/app.js
+++ section03/src/app.js
@@ -1,14 +1,8 @@
-'use strict';
-
 /* app.js */
 
 console.log('app.js engage');
 
-var template = React.createElement(
-  'p',
-  null,
-  'This is JSX from app.js!'
-);
+var template = <p>This is JSX from app.js!</p>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
