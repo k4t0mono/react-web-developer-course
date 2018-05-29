9a057760edab506b91bdb88001cc1eeaed4ab047
KatoMono Enkeli <k4t0mono@gmail.com>
2018-05-29 17:33:40 -0300

    Lecture 026: Creating a React Component

---
+++ section04/src/app.jsx
@@ -0,0 +1,57 @@
+// app.jsx
+
+class Header extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Indecision</h1>
+				<h2>Put your life in the hands of a computer</h2>
+			</div>
+		);
+	}
+}
+
+
+class Action extends React.Component {
+	render() {
+		return (
+			<div>
+				<button>What sould I do?</button>
+			</div>
+		);
+	}
+}
+
+
+class Options extends React.Component {
+	render() {
+		return (
+			<div>
+				Options Component
+			</div>
+		);
+	}
+}
+
+
+class AddOption extends React.Component {
+	render() {
+		return (
+			<div>
+				AddOption Component
+			</div>
+		)
+	}
+}
+
+
+const jsx = (
+	<div>
+		<Header />
+		<Action />
+		<Options />
+		<AddOption />
+	</div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
