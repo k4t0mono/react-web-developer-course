2f8e04e80b4d2d49ad60c9933f06a1531b39b61b
KatoMono Enkeli <k4t0mono@gmail.com>
2018-09-02 17:38:23 -0300

    Refactoring

--- section07/src/components/action.jsx
+++ section07/src/components/action.jsx
@@ -3,15 +3,13 @@
 import React from 'react';
 
 
-const Action = (props) => {
-	return (
-		<div>
-			<button onClick={ props.handlePick } disabled={ !props.hasOption }>
-				What sould I do?
-			</button>
-		</div>
-	);
-};
+const Action = (props) => (
+	<div>
+		<button onClick={ props.handlePick } disabled={ !props.hasOption }>
+			What sould I do?
+		</button>
+	</div>
+);
 
 
 export default Action;
--- section07/src/components/options.jsx
+++ section07/src/components/options.jsx
@@ -5,21 +5,19 @@ import React from 'react';
 import Option from './option.jsx';
 
 
-const Options = (props) => {
-	return (
-		<div>
-			<button onClick={ props.handleDeleteOptions }>Remove All</button>
-			{ !props.options.length && <p>Please add an option to start</p> }
+const Options = (props) => (
+	<div>
+		<button onClick={ props.handleDeleteOptions }>Remove All</button>
+		{ !props.options.length && <p>Please add an option to start</p> }
 
-			{ props.options.map((op) => (
-				<Option
-					key={ op }
-					optionText={ op }
-					handleDeleteOption={ props.handleDeleteOption } 
-				/>
-			)) }
-		</div>
-	);
-};
+		{ props.options.map((op) => (
+			<Option
+				key={ op }
+				optionText={ op }
+				handleDeleteOption={ props.handleDeleteOption } 
+			/>
+		)) }
+	</div>
+);
 
 export default Options;
