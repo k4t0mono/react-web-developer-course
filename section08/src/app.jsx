43c1634e9f4c2d934a42209499a8581314703fd9
KatoMono Enkeli <k4t0mono@gmail.com>
2018-09-02 20:00:24 -0300

    Lecture 066: Reset that $#!*

--- section08/src/app.jsx
+++ section08/src/app.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/indecisionApp.jsx';
+import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
