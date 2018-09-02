1448210b91d18155c3d19614d538b304f426d308
KatoMono Enkeli <k4t0mono@gmail.com>
2018-09-02 18:04:16 -0300

    Lecture 064: Setting up Wbpack with SCSS

--- section08/src/app.jsx
+++ section08/src/app.jsx
@@ -4,6 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/indecisionApp.jsx';
-
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
