447e11e33df5f8aa898d8d22f0098aa880a6e18d
KatoMono Enkeli <k4t0mono@gmail.com>
2018-09-20 23:53:18 -0300

    Lecture 78: Setting up 404

--- section09/src/app.jsx
+++ section09/src/app.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
@@ -32,9 +32,15 @@ const HelpPage = () => (
 	</div>
 );
 
+const NotFoundPage = () => (
+	<div>
+		404
+	</div>
+);
+
 const routes = (
 	<BrowserRouter>
-		<div>
+		<Switch>
 			<Route
 				path='/'
 				component={ ExpenseDashboardPage }
@@ -55,7 +61,11 @@ const routes = (
 				path='/help'
 				component={ HelpPage }
 			/>
-		</div>
+
+			<Route
+				component={ NotFoundPage }
+			/>
+		</Switch>
 	</BrowserRouter>
 );
 
