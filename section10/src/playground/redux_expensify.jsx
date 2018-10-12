a39057cc25137709ae637769022487ec868119d9
KatoMono Enkeli <k4t0mono@gmail.com>
2018-10-11 21:36:59 -0300

    Lecture 093: ES6 spread operator in reducers

--- section10/src/playground/redux_expensify.jsx
+++ section10/src/playground/redux_expensify.jsx
@@ -1,10 +1,32 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
+const add_expense = ({ description='', note='', amount=0, created_at=0 }) => ({
+	type: 'add_expense',
+	expense: {
+		id: uuid(),
+		description,
+		note,
+		amount,
+		created_at,
+	}
+});
+
+const remove_expense = ({ id } = {}) => ({
+	type: 'remove_expense',
+	id
+});
 
 const expenses_reducer = (state = [], action) => {
 	switch (action.type) {
-	default:
-		return state;
+		case 'add_expense':
+			return [...state, action.expense];
+
+		case 'remove_expense':
+			return state.filter(({ id }) => id !== action.id );
+
+		default:
+			return state;
 	}
 }
 
@@ -17,8 +39,8 @@ const filter_default_state = {
 
 const filter_reducer = (state = filter_default_state, action) => {
 	switch(action) {
-	default:
-		return state;
+		default:
+			return state;
 	}
 }
 
@@ -29,7 +51,15 @@ const store = createStore(
 	})
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+const e0 = store.dispatch(add_expense({ description: 'docinho', amount: 1 }));
+const e1 = store.dispatch(add_expense({ description: 'caipirinha', amount: 11.5 }));
+
+console.log(e0.expense.id);
+store.dispatch(remove_expense({ id: e0.expense.id }));
 
 const demoState = {
 	expenses: [{
