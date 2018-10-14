2b6d49c9b42603bec9fd5db0331463c28291845a
KatoMono Enkeli <k4t0mono@gmail.com>
2018-10-14 17:36:37 -0300

    Lecture 095: Wrapping up reducers

--- section10/src/playground/redux_expensify.jsx
+++ section10/src/playground/redux_expensify.jsx
@@ -25,25 +25,25 @@ const edit_expense = (id, updates) => ({
 
 const expenses_reducer = (state = [], action) => {
 	switch (action.type) {
-		case 'add_expense':
-			return [...state, action.expense];
+	case 'add_expense':
+		return [...state, action.expense];
 
-		case 'remove_expense':
-			return state.filter(({ id }) => id !== action.id );
+	case 'remove_expense':
+		return state.filter(({ id }) => id !== action.id );
 
-		case 'edit_expense':
-			return state.map((expense) => {
-				if(expense.id == action.id)
-					return {
-						...expense,
-						...action.updates,
-					}
+	case 'edit_expense':
+		return state.map((expense) => {
+			if(expense.id == action.id)
+				return {
+					...expense,
+					...action.updates,
+				}
 
-				return expense
-			});
+			return expense
+		});
 
-		default:
-			return state;
+	default:
+		return state;
 	}
 }
 
@@ -51,6 +51,24 @@ const expenses_reducer = (state = [], action) => {
 const set_text_filter = (text = '') => ({
 	type: 'set_text_filter',
 	text,
+});
+
+const sort_by_amount = () => ({
+	type: 'sort_by_amount'
+});
+
+const sort_by_date = () => ({
+	type: 'sort_by_date'
+});
+
+const set_start_date = (start_date) => ({
+	type: 'set_start_date',
+	start_date,
+});
+
+const set_end_date = (end_date) => ({
+	type: 'set_end_date',
+	end_date,
 });
 
 const filter_default_state = {
@@ -62,14 +80,32 @@ const filter_default_state = {
 
 const filter_reducer = (state = filter_default_state, action) => {
 	switch(action.type) {
-		case 'set_text_filter':
-			return {
-				...state,
-				text: action.text,
-			}
+	case 'set_text_filter':
+		return {
+			...state,
+			text: action.text,
+		}
 
-		default:
-			return state;
+	case 'sort_by_amount':
+		return {
+			...state,
+			sort_by: 'amount',
+		}
+
+	case 'set_start_date':
+		return {
+			...state,
+			start_date: action.start_date,
+		}
+
+	case 'set_end_date':
+		return {
+			...state,
+			end_date: action.end_date,
+		}
+
+	default:
+		return state;
 	}
 }
 
@@ -85,12 +121,18 @@ store.subscribe(() => {
 	console.log(store.getState());
 });
 
+/*
 const e0 = store.dispatch(add_expense({ description: 'docinho', amount: 1 }));
 const e1 = store.dispatch(add_expense({ description: 'caipirinha', amount: 11.5 }));
 
 store.dispatch(remove_expense({ id: e0.expense.id }));
 store.dispatch(edit_expense(e1.expense.id, { amount: 5 }));
 store.dispatch(set_text_filter('asdf'));
+store.dispatch(sort_by_amount());
+*/
+
+store.dispatch(set_start_date(123));
+store.dispatch(set_end_date(153));
 
 const demoState = {
 	expenses: [{
